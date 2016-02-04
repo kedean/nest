dc93757e9b603c7f847411926d2046bc693e9441
Kevin Dean <kevinedean@gmail.com>
2016-02-03 21:29:33 -0600

    implemented all http verbs

--- nest.nim
+++ nest.nim
@@ -16,13 +16,14 @@ proc newNestServer* () : NestServer =
   let routing = newRouter()
 
   proc dispatch(req: Request) {.async, gcsafe.} =
+    let requestMethod = req.reqMethod
     let requestPath = req.url.path
     let queryString = req.url.query
-    let (handler, params) = routing.match(requestPath, queryString)
+    let (handler, params) = routing.match(requestMethod, requestPath, queryString)
 
     if handler == nil:
       let fullPath = requestPath & (if queryString.len() > 0: "?" & queryString else: "")
-      echo "No mapping found for path '", fullPath, "'"
+      echo "No mapping found for path '", fullPath, "' with method '", requestMethod, "'"
       await req.respond(Http404, "Resource not found!")
     else:
       let content = handler(req, params)
@@ -37,8 +38,8 @@ proc newNestServer* () : NestServer =
 proc run*(nest : NestServer, portNum : int) =
   waitFor nest.httpServer.serve(Port(portNum), nest.dispatchMethod)
 
-proc addRoute*(nest : NestServer, requestPath : string, handler : RequestHandler) =
-  nest.router.route(requestPath, handler)
+proc addRoute*(nest : NestServer, requestMethod : string, requestPath : string, handler : RequestHandler) =
+  nest.router.route(requestMethod, requestPath, handler)
 
 template onPort*(portNum, actions: untyped): untyped =
   let server {.inject.} = newNestServer()
@@ -48,9 +49,54 @@ template onPort*(portNum, actions: untyped): untyped =
   finally:
     discard
 
-template map*(path, actions:untyped) : untyped =
-  server.addRoute(path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
-template map*(path, request, actions:untyped) : untyped =
-  server.addRoute(path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
-template map*(path, request, params, actions:untyped) : untyped =
-  server.addRoute(path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+# Templates to simplify writing handlers
+
+const
+  GET = "get"
+  POST = "post"
+  HEAD = "head"
+  OPTIONS = "options"
+  PUT = "put"
+  DELETE = "delete"
+
+template get*(path, actions:untyped) : untyped =
+  server.addRoute(GET, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template get*(path, request, actions:untyped) : untyped =
+  server.addRoute(GET, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template get*(path, request, params, actions:untyped) : untyped =
+  server.addRoute(GET, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+
+template post*(path, actions:untyped) : untyped =
+  server.addRoute(POST, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template post*(path, request, actions:untyped) : untyped =
+  server.addRoute(POST, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template post*(path, request, params, actions:untyped) : untyped =
+  server.addRoute(POST, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+
+template head*(path, actions:untyped) : untyped =
+  server.addRoute(HEAD, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template head*(path, request, actions:untyped) : untyped =
+  server.addRoute(HEAD, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template head*(path, request, params, actions:untyped) : untyped =
+  server.addRoute(HEAD, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+
+template options*(path, actions:untyped) : untyped =
+  server.addRoute(OPTIONS, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template options*(path, request, actions:untyped) : untyped =
+  server.addRoute(OPTIONS, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template options*(path, request, params, actions:untyped) : untyped =
+  server.addRoute(OPTIONS, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+
+template put*(path, actions:untyped) : untyped =
+  server.addRoute(PUT, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template put*(path, request, actions:untyped) : untyped =
+  server.addRoute(PUT, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template put*(path, request, params, actions:untyped) : untyped =
+  server.addRoute(PUT, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+
+template delete*(path, actions:untyped) : untyped =
+  server.addRoute(DELETE, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template delete*(path, request, actions:untyped) : untyped =
+  server.addRoute(DELETE, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
+template delete*(path, request, params, actions:untyped) : untyped =
+  server.addRoute(DELETE, path, proc (request:Request, params:Params) : string {.gcsafe.} = actions)
